308cf54c2f269181b12c3361e831a0ff005a5a5e
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2021-01-30 05:03:16 +0100

    Implement CollidableBehavior

--- Assets/Scripts/Core/Component/Doodad/CollidableBehaviour.cs
+++ Assets/Scripts/Core/Component/Doodad/CollidableBehaviour.cs
@@ -2,8 +2,27 @@
 
 namespace Core.Component.Doodad
 {
-    public class CollidableBehaviour : MonoBehaviour
+    public abstract class CollidableBehaviour : MonoBehaviour
     {
-        
+        protected abstract void OnCollision(GameObject other);
+
+        protected abstract void OnCollisionLeave(GameObject other);
+
+        protected abstract void OnCollisionUpdate(GameObject other);
+
+        private void OnCollisionEnter(Collision other)
+        {
+            OnCollision(other.gameObject);
+        }
+
+        private void OnCollisionStay(Collision other)
+        {
+            OnCollisionUpdate(other.gameObject);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            OnCollisionLeave(other.gameObject);
+        }
     }
 }
