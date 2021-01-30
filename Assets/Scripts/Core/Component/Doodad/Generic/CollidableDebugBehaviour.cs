322431a6e2970a74b8ca390420173187b1e364eb
Borjan Gjorovski <borjan.gjorovski@outlook.com>
2021-01-30 05:23:01 +0100

    Complete collidable behaviour

---
+++ Assets/Scripts/Core/Component/Doodad/Generic/CollidableDebugBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Core.Component.Doodad.Generic
+{
+    public class CollidableDebugBehaviour : CollidableBehaviour
+    {
+        protected override void OnCollision(GameObject other)
+        {
+            Debug.Log($"Collision: {other.name}");
+        }
+
+        protected override void OnCollisionLeave(GameObject other)
+        {
+            Debug.Log($"Collision Leave: {other.name}");
+        }
+
+        protected override void OnCollisionUpdate(GameObject other)
+        {
+            Debug.Log($"Colliding: {gameObject.name}");
+        }
+    }
+}
